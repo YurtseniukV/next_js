d8fc6b9735a18c7c7a8b549de319ae8939627f93
YurtseniukV <virayurtseniuk@gmail.com>
2024-06-29 20:56:29 +0300

    HW1 - nextjs

---
+++ src/app/comments/page.tsx
@@ -0,0 +1,12 @@
+import CommentsComponent from '@/components/comments-component/CommentsComponent';
+import React from 'react';
+
+const CommentsPage = () => {
+    return (
+        <div>
+           <CommentsComponent/>
+        </div>
+    );
+};
+
+export default CommentsPage;
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,13 @@
+import PostsComponent from '@/components/posts-component/PostsComponent';
+import React from 'react';
+
+const PostsPage = () => {
+
+    return (
+        <div>
+          <PostsComponent/>
+        </div>
+    );
+};
+
+export default PostsPage;
